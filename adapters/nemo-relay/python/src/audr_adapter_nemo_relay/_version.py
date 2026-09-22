204a7e0283c6d408f8561d7e521c0c2c5d604028
Dinesh <dinesh@chargebee.com>
2026-09-22 11:39:54 +0530

    chore(release): prepare 0.1.0 release for python packages

--- adapters/nemo-relay/python/src/audr_adapter_nemo_relay/_version.py
+++ adapters/nemo-relay/python/src/audr_adapter_nemo_relay/_version.py
@@ -1,3 +1,3 @@
 """Single source for the NeMo Relay adapter version."""
 
-__version__ = "0.1.0a1"
+__version__ = "0.1.0"
--- sinks/chargebee/python/src/audr_sink_chargebee/_version.py
+++ sinks/chargebee/python/src/audr_sink_chargebee/_version.py
@@ -1,3 +1,3 @@
 """Single source for the Chargebee sink version."""
 
-__version__ = "0.1.0a1"
+__version__ = "0.1.0"
